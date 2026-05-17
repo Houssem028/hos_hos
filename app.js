6763cf1b970ef143ccc6309866d62d9bf7b9210a
Hos_Hos <109749794+Houssem028@users.noreply.github.com>
2026-05-17 13:56:54 +0100

    Remove like function and add login functionality

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-function like() {
-  alert("❤️ Liked on Hos Hos");
+setTimeout(() => {
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("login").classList.remove("hidden");
+}, 3000);
+
+function login(){
+  alert("تم تسجيل الدخول إلى Hos Hos");
 }
