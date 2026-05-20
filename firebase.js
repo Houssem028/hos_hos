68fd6d72b86e890ca7adaaaa131205f5171a5843
Hos_Hos <109749794+Houssem028@users.noreply.github.com>
2026-05-20 17:41:24 +0100

    Implement user authentication functions
    Add signup, login, and logout functions for user authentication.

--- firebase.js
+++ firebase.js
@@ -629,3 +629,81 @@ b.updatedAt - a.updatedAt
 );
 
   }
+/* ================= AUTH ================= */
+
+/* SIGNUP */
+
+export async function signup(
+email,
+password,
+username
+){
+
+const userCredential =
+await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+const user =
+userCredential.user;
+
+await setDoc(
+doc(db,"users",user.uid),
+{
+uid:user.uid,
+
+username,
+
+image:"",
+
+verified:false,
+
+followers:0,
+
+following:0,
+
+followersList:[],
+
+followingList:[],
+
+createdAt:Date.now()
+}
+);
+
+return user;
+
+}
+
+/* LOGIN */
+
+export async function login(
+email,
+password
+){
+
+const userCredential =
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+return userCredential.user;
+
+}
+
+/* LOGOUT */
+
+export async function logout(){
+
+await signOut(auth);
+
+}
+
+/* AUTH STATE */
+
+export {
+onAuthStateChanged
+};
